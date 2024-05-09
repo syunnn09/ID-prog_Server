ceb1386ff6b22a76150ce61eae130736e62dec1d
syunnn <syusuke5103@gmail.com>
2024-05-09 15:15:15 +0900

    [fix] api

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
 from flask_cors import CORS
+import subprocess
 
 app = Flask(__name__)
 cors = CORS(app, resources={r'/*': {'origins': 'http://localhost:5173'}})
+
+def write(data):
+    with open('data.py', 'w', encoding='utf-8') as f:
+        f.write(data)
 
 @app.route('/')
 def index():
@@ -10,7 +15,11 @@ def index():
 
 @app.route('/post', methods=['GET', 'POST'])
 def post():
-    print(request.get_data())
-    return 'ok'
+    write(request.json['data'])
+    ret = subprocess.run("py data.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    return {
+        'res': ret.stdout,
+        'err': ret.stderr
+    }
 
 app.run('localhost', 55555, debug=True)
---
+++ data.py
@@ -0,0 +1,6 @@
+
+for j in range(3):
+	print(j)
+print(i)
+
+
